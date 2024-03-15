ce9dbe48bed8237d670f5ea1cd60073c65c7d5c3
Mohib <mohibullahmohim2020@gmail.com>
2024-03-15 15:42:29 +0600

    aktu khani roice

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 function App() {
 
   const [ books, setBooks ] = useState( [] );
-
+  const [ cartItem, setCartItem ] = useState( [] )
   // browse Data 
 
   useEffect( () => {
@@ -15,7 +15,15 @@ function App() {
     .then(data => setBooks(data))
   }, [] );
 
-  console.log(books);
+  // console.log(books);
+
+  // console.log(cartItem, 'before added');
+
+  const addToCart = (bookCart) => {
+    // console.log('clicked button', bookCart );
+    setCartItem([ ...cartItem, bookCart]);
+  };
+  console.log(cartItem, 'new list');
 
   return (
     <>
@@ -29,60 +37,57 @@ function App() {
       {
         books.map((book) => {
           return (
-            <div key={book.id}>
+            <div  key={book.bookImage} className='mx-auto flex'>
+              <div className=" w-[ 65% ] ">
+                <div className="">
 
-              <div className='grid grid-cols-4 '>
-                <div className='flex col-span-3 '> 
-
-                <div className="grid grid-cols-3">
-
-                {/* Book Card */}
-                <div className=''>
+                  
                 <div className="card card-compact bg-base-100 shadow-xl">
-                  <figure><img src= {book.bookImage} alt="Shoes" /></figure>
-                  <div className="card-body">
-                    <h2 className="card-title"> {book.bookTitle}</h2>
-                    <h3 className="card-title"> {book.bookAuthor}</h3>
-                    <p className="card-title"> {book.bookPrice} </p>
-                    <p>{ book.bookStatus }</p>
-                    <div className="card-actions justify-end">
-                      <button className="btn bg-gray-300 text-white p-3 rounded-lg">Buy Now</button>
+                    <figure><img src={book.bookImage} alt="Book" /></figure>
+                    <div className="card-body">
+                      <h2 className="card-title">{book.bookTitle}</h2>
+                      <h3 className="card-title">{book.bookAuthor}</h3>
+                      <p className="card-title">{book.bookPrice}</p>
+                      <p>{book.bookStatus}</p>
+                      <div className="card-actions justify-end">
+                        <button onClick={() => addToCart(book)} className="btn bg-gray-400 text-white p-3 rounded-lg">Buy Now</button>
+                      </div>
                     </div>
                   </div>
+                  
+                  <div className="">
+                    <div className=""></div>
+                  </div>
                 </div>
-                </div>
-                {/* Book Card */}
-                </div>
+              </div>
 
-                </div>
-          
-              <div className='it wil bg-blue-200 '>
-                <div className='flex justify-around' >
-                  <h3>Name</h3>
-                  <h3>Price</h3>
-                </div>
-            
-              <div>
+              <div className=" w-[ 35% ] ">
+              <div className="bg-blue-200">
+                <div>
+                  <div className="flex justify-around">
+                    <h3>Name</h3>
+                    <h3>Price</h3>
+                  </div>
 
-                <div className='flex justify-around' >
-                  <h3>Name</h3>
-                  <h3>500</h3>
+                    {
+                      cartItem?.map( item => {
+                        return ( 
+                          <div key={item.bookImage} className="flex justify-around">
+                            <h2>{item.bookTitle} </h2>
+                            <h2>{item.bookPrice} </h2>
+                          </div>
+                         )
+                      } )
+                    }
                 </div>
-                
-                <div className='flex justify-around' >
-                  <h3>Name</h3>
-                  <h3>500</h3>
-                </div>
+              </div>
 
               </div>
-          </div>
-      </div>
-
-
             </div>
           );
         })
       }
+
 
 
     </>
