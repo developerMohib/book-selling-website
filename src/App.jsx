2847b530fe6ffbfe36406e8b7865c09d64d9cfb3
Mohib <mohibullahmohim2020@gmail.com>
2024-03-15 12:54:49 +0600

    just design added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
@@ -13,7 +13,69 @@ function App() {
       </div>
       {/* My Working Place Here */}
 
+      <div className='grid grid-cols-4 '>
+          <div className='flex col-span-3 '> 
+           
+           {/* Book Card */}
+           <div>
+           <div className="card card-compact bg-base-100 shadow-xl">
+            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn bg-gray-300 text-white p-3 rounded-lg">Buy Now</button>
+              </div>
+            </div>
+          </div>
+           </div>
+           {/* Book Card */}
 
+           {/* Book Card */}
+           <div>
+           <div className="card card-compact bg-base-100 shadow-xl">
+            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn bg-gray-300 text-white p-3 rounded-lg">Buy Now</button>
+              </div>
+            </div>
+          </div>
+           </div>
+           {/* Book Card */}
+           {/* Book Card */}
+           <div>
+           <div className="card card-compact bg-base-100 shadow-xl">
+            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn bg-gray-300 text-white p-3 rounded-lg">Buy Now</button>
+              </div>
+            </div>
+          </div>
+           </div>
+           {/* Book Card */}
+
+
+          </div>
+          
+          <div className='it wil bg-blue-200 '>
+            <div className='flex justify-around' >
+              <h3>Name</h3>
+              <h3>Price</h3>
+            </div>
+            
+            <div className='flex justify-around' >
+              <h3>Name</h3>
+              <h3>500</h3>
+            </div>
+
+          </div>
+      </div>
 
     </>
   )
