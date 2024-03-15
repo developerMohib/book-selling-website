ef81571a033462dae939b85419986e1addac388c
Mohib <mohibullahmohim2020@gmail.com>
2024-03-15 14:32:05 +0600

    Design done

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,89 @@
 
+import { useEffect } from 'react'
 import './App.css'
+import { useState } from 'react';
 
 function App() {
 
+  const [ books, setBooks ] = useState( [] );
+
+  // browse Data 
+
+  useEffect( () => {
+    fetch('book.json')
+    .then(res => res.json())
+    .then(data => setBooks(data))
+  }, [] );
+
+  console.log(books);
 
   return (
     <>
       <div>
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        My Books Selling Website !
       </h1>
       </div>
       {/* My Working Place Here */}
 
-      <div className='grid grid-cols-4 '>
-          <div className='flex col-span-3 '> 
-           
-           {/* Book Card */}
-           <div>
-           <div className="card card-compact bg-base-100 shadow-xl">
-            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-            <div className="card-body">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn bg-gray-300 text-white p-3 rounded-lg">Buy Now</button>
-              </div>
-            </div>
-          </div>
-           </div>
-           {/* Book Card */}
+      {
+        books.map((book) => {
+          return (
+            <div key={book.id}>
 
-           {/* Book Card */}
-           <div>
-           <div className="card card-compact bg-base-100 shadow-xl">
-            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-            <div className="card-body">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn bg-gray-300 text-white p-3 rounded-lg">Buy Now</button>
-              </div>
-            </div>
-          </div>
-           </div>
-           {/* Book Card */}
-           {/* Book Card */}
-           <div>
-           <div className="card card-compact bg-base-100 shadow-xl">
-            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-            <div className="card-body">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn bg-gray-300 text-white p-3 rounded-lg">Buy Now</button>
-              </div>
-            </div>
-          </div>
-           </div>
-           {/* Book Card */}
+              <div className='grid grid-cols-4 '>
+                <div className='flex col-span-3 '> 
 
+                <div className="grid grid-cols-3">
 
-          </div>
+                {/* Book Card */}
+                <div className=''>
+                <div className="card card-compact bg-base-100 shadow-xl">
+                  <figure><img src= {book.bookImage} alt="Shoes" /></figure>
+                  <div className="card-body">
+                    <h2 className="card-title"> {book.bookTitle}</h2>
+                    <h3 className="card-title"> {book.bookAuthor}</h3>
+                    <p className="card-title"> {book.bookPrice} </p>
+                    <p>{ book.bookStatus }</p>
+                    <div className="card-actions justify-end">
+                      <button className="btn bg-gray-300 text-white p-3 rounded-lg">Buy Now</button>
+                    </div>
+                  </div>
+                </div>
+                </div>
+                {/* Book Card */}
+                </div>
+
+                </div>
           
-          <div className='it wil bg-blue-200 '>
-            <div className='flex justify-around' >
-              <h3>Name</h3>
-              <h3>Price</h3>
-            </div>
+              <div className='it wil bg-blue-200 '>
+                <div className='flex justify-around' >
+                  <h3>Name</h3>
+                  <h3>Price</h3>
+                </div>
             
-            <div className='flex justify-around' >
-              <h3>Name</h3>
-              <h3>500</h3>
-            </div>
+              <div>
 
+                <div className='flex justify-around' >
+                  <h3>Name</h3>
+                  <h3>500</h3>
+                </div>
+                
+                <div className='flex justify-around' >
+                  <h3>Name</h3>
+                  <h3>500</h3>
+                </div>
+
+              </div>
           </div>
       </div>
+
+
+            </div>
+          );
+        })
+      }
+
 
     </>
   )
